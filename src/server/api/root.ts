90a05f70e4d56644d7746abee3f583b446941d30
Benjamin Davis <davis.benjamin41902@gmail.com>
2024-05-23 01:40:00 -0400

    created home shell

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
-import { postRouter } from "@/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
+import { foodsRouter } from "./routers/foods";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +7,7 @@ import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  foods: foodsRouter,
 });
 
 // export type definition of API
---
+++ src/server/api/routers/foods.ts
@@ -0,0 +1,3 @@
+import { createTRPCRouter } from "../trpc";
+
+export const foodsRouter = createTRPCRouter({});
